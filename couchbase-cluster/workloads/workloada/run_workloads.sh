d49225f35acd64b130e893cb9a7fcde547bd813c
kruthar <kruthar@gmail.com>
2015-02-16 11:33:59 -0600

    initial commit of working vagrant provisioning of couchbase cluster as well as a sample workload for a couchbase cluster

---
+++ couchbase-cluster/workloads/workloada/run_workloads.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Script to run a workload on a couchbase cluster
+
+DATE=$(date +"%Y%m%d%H%M")
+workload=workloada
+bucket=benchmark
+user=couchbase
+password=couchbase
+ram=1024
+replicas=2
+
+PATH=$PATH:~/Projects/YCSB/bin
+
+vagrant ssh -c "/opt/couchbase/bin/couchbase-cli bucket-delete -c 127.0.0.1:8091 --bucket=$bucket -u $user -p $password" cb1.benchmark
+sleep 2
+vagrant ssh -c "/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=$bucket --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=$ram  --bucket-replica=$replicas -u $user -p $password" cb1.benchmark
+sleep 2
+curl -X PUT -H 'Content-Type: application/json' http://couchbase:couchbase@cb1.benchmark:8092/$bucket/_design/indexes -d @workloada.ddoc
+sleep 2
+ycsb load couchbase -P $workload.properties -P couchbase.properties -s > results/$workload-load-$DATE.txt
+ycsb run couchbase -P $workload.properties -P couchbase.properties -s > results/$workload-run-$DATE.txt
