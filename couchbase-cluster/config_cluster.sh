b2dfd1c62d58b085eadfb0cd831410602b3fc81b
kruthar <kruthar@gmail.com>
2015-02-16 12:49:03 -0600

    added ganglia to the couchbase provisioning and changed the cluster and bucket init sizes to 512m so they will fit on small 1G servers

--- couchbase-cluster/config_cluster.sh
+++ couchbase-cluster/config_cluster.sh
@@ -2,13 +2,10 @@
 set -e
 
 vagrant up
-vagrant ssh -c "/opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091 --cluster-init-username=couchbase --cluster-init-password=couchbase --cluster-init-port=8091 --cluster-init-ramsize=1024" cb1.benchmark
+vagrant ssh -c "/opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091 --cluster-init-username=couchbase --cluster-init-password=couchbase --cluster-init-port=8091 --cluster-init-ramsize=512" cb1.benchmark
 sleep 1
 vagrant ssh -c "/opt/couchbase/bin/couchbase-cli server-add -c 127.0.0.1:8091 -u couchbase -p couchbase --server-add=cb2.benchmark:8091 --server-add-username=couchbase --server-add-password=couchbase" cb1.benchmark
 sleep 1
 vagrant ssh -c "/opt/couchbase/bin/couchbase-cli server-add -c 127.0.0.1:8091 -u couchbase -p couchbase --server-add=cb3.benchmark:8091 --server-add-username=couchbase --server-add-password=couchbase" cb1.benchmark
 sleep 1
 vagrant ssh -c "/opt/couchbase/bin/couchbase-cli rebalance -c 127.0.0.1:8091 -u couchbase -p couchbase" cb1.benchmark
-vagrant ssh -c "/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=benchmark --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=1024  --bucket-replica=2 -u couchbase -p couchbase" cb1.benchmark
-sleep 1
-curl -X PUT -H 'Content-Type: application/json' http://couchbase:couchbase@cb1.benchmark:8092/benchmark/_design/indexes -d @workload_a.ddoc
--- couchbase-cluster/workloads/workloada/run_workloads.sh
+++ couchbase-cluster/workloads/workloada/run_workloads.sh
@@ -7,10 +7,13 @@ workload=workloada
 bucket=benchmark
 user=couchbase
 password=couchbase
-ram=1024
+ram=512
 replicas=2
 
 PATH=$PATH:~/Projects/YCSB/bin
+
+# We are dealing with local files so put the user in the right dir
+cd ${0%/*}
 
 vagrant ssh -c "/opt/couchbase/bin/couchbase-cli bucket-delete -c 127.0.0.1:8091 --bucket=$bucket -u $user -p $password" cb1.benchmark
 sleep 2
@@ -18,5 +21,10 @@ vagrant ssh -c "/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091
 sleep 2
 curl -X PUT -H 'Content-Type: application/json' http://couchbase:couchbase@cb1.benchmark:8092/$bucket/_design/indexes -d @workloada.ddoc
 sleep 2
+
+if [ ! -d "results" ]; then
+	mkdir results
+fi
+
 ycsb load couchbase -P $workload.properties -P couchbase.properties -s > results/$workload-load-$DATE.txt
 ycsb run couchbase -P $workload.properties -P couchbase.properties -s > results/$workload-run-$DATE.txt
